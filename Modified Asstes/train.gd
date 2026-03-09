fcd5c906d0e04a12d4c8b1bfccda21363fe51d96
Udeagha Chukwubuikem Xavier <danonymousuchiha@gmail.com>
2026-03-09 22:53:46 +0100

    Add train window lights & spawn logic
    Introduce window light component and automatic spawning for the train. Adds a new SpotLight3D scene (Scenes/Components/window_light.tscn) and a train script (Modified Asstes/train.gd + uid) that spawns instances of the window light at marker positions (LightsMarkersFront / LightsMarkersBack) under a Light container in the train scene. Register the script on the Train node in Modified Asstes/train.tscn. Update Autoload/Cache.gd to preload the train_window_light_scene for easy instantiation. Also include a small inline comment change in Scenes/Entities/player.gd about SPEED and update Scenes/world.res (binary) to reflect scene edits.

---
+++ Modified Asstes/train.gd
@@ -0,0 +1,24 @@
+extends Node3D
+
+@onready var lights_markers_front: Node3D = $LightsMarkersFront
+@onready var lights_markers_back: Node3D = $LightsMarkersBack
+
+@onready var lights_container: Node3D = $Light
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	spawn_lights_at_windows()
+
+
+func spawn_lights_at_windows():
+	for marker in lights_markers_front.get_children():
+		var light_scene = Cache.train_window_light_scene.instantiate()
+		lights_container.add_child(light_scene)
+		light_scene.global_position = marker.global_position
+		
+	for marker in lights_markers_back.get_children():
+		var light_scene = Cache.train_window_light_scene.instantiate()
+		lights_container.add_child(light_scene)
+		light_scene.global_position = marker.global_position
+		
+		
